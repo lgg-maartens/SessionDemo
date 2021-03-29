a75899432427f565b8c3ee43e6da65c8a4365332
Tim Logtenberg <tplogtenberg@gmail.com>
2021-03-29 17:26:19 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionDemo.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      ViewData["user"] = HttpContext.Session.GetString("User");
+
+      return View();
+    }
+
+    public IActionResult Login(string username, string password)
+    {
+      if (password == "geheim")
+      {
+        HttpContext.Session.SetString("User", username);
+        return Redirect("/");
+      }
+
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
