425be9380ac44a94179c65091b84ba9609f3949a
Tim Logtenberg <tplogtenberg@gmail.com>
2021-06-02 11:27:43 +0200

    Added store method for storing person data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using SessionDemo.Models;
 using System.Diagnostics;
 using System.Security.Cryptography;
@@ -48,6 +49,26 @@ namespace SessionDemo.Controllers
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    private void SavePerson(Person person)
+    {
+      // voordat we alles opslaan in de database gaan we eerst het wachtwoord hashen
+      person.Password = ComputeSha256Hash(person.Password);
+      
+      using (MySqlConnection conn = new MySqlConnection("connectionstring vul je hier in"))
+      {
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand("INSERT INTO klant(voornaam, achternaam, wachtwoord, email, bericht) " +
+          "VALUES(?voornaam, ?achternaam, ?wachtwoord, ?email, ?bericht)", conn);
+
+        cmd.Parameters.Add("?voornaam", MySqlDbType.Text).Value = person.FirstName;
+        cmd.Parameters.Add("?achternaam", MySqlDbType.Text).Value = person.LastName;
+        cmd.Parameters.Add("?wachtwoord", MySqlDbType.Text).Value = person.Password;
+        cmd.Parameters.Add("?email", MySqlDbType.Text).Value = person.Email;
+        cmd.Parameters.Add("?bericht", MySqlDbType.Text).Value = person.Description;
+        cmd.ExecuteNonQuery();
+      }
     }
 
     static string ComputeSha256Hash(string rawData)
---
+++ Controllers/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace SessionDemo.Controllers
+{
+  internal class Person
+  {
+    public string FirstName { get; internal set; }
+    public string LastName { get; internal set; }
+    public string Email { get; internal set; }
+    public string Description { get; internal set; }
+    public string Password { get; internal set; }
+  }
+}
